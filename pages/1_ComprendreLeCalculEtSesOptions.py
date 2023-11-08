635b5acf0d581ae7be489cf51a61143c86fe72b3
Mathieu Bellanger <70530223+Cinetielle@users.noreply.github.com>
2023-11-08 23:03:25 +0000

    modified:   pages/1_ComprendreLeCalculEtSesOptions.py 	main branche commits squashed

--- pages/1_ComprendreLeCalculEtSesOptions.py
+++ pages/1_ComprendreLeCalculEtSesOptions.py
@@ -20,37 +20,39 @@ import streamlit as st
 from streamlit.hello.utils import show_code
 
 
-def plotting_demo():
-    progress_bar = st.sidebar.progress(0)
-    status_text = st.sidebar.empty()
-    last_rows = np.random.randn(1, 1)
-    chart = st.line_chart(last_rows)
+# def plotting_demo():
+#     progress_bar = st.sidebar.progress(0)
+#     status_text = st.sidebar.empty()
+#     last_rows = np.random.randn(1, 1)
+#     chart = st.line_chart(last_rows)
 
-    for i in range(1, 101):
-        new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-        status_text.text("%i%% Complete" % i)
-        chart.add_rows(new_rows)
-        progress_bar.progress(i)
-        last_rows = new_rows
-        time.sleep(0.05)
+#     for i in range(1, 101):
+#         new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+#         status_text.text("%i%% Complete" % i)
+#         chart.add_rows(new_rows)
+#         progress_bar.progress(i)
+#         last_rows = new_rows
+#         time.sleep(0.05)
 
-    progress_bar.empty()
+#     progress_bar.empty()
 
-    # Streamlit widgets automatically run the script from top to bottom. Since
-    # this button is not connected to any other logic, it just causes a plain
-    # rerun.
-    st.button("Re-run")
+#     # Streamlit widgets automatically run the script from top to bottom. Since
+#     # this button is not connected to any other logic, it just causes a plain
+#     # rerun.
+#     st.button("Re-run")
 
 
 st.set_page_config(page_title="Le calcul et ses options", page_icon="📈")
 st.markdown("# Le calcul et ses options")
 st.sidebar.header("Paramètres")
 st.write(
-    """This demo illustrates a combination of plotting and animation with
-Streamlit. We're generating a bunch of random numbers in a loop for around
-5 seconds. Enjoy!"""
+    """Nous allons voir ici comment le code fonctionne et quels sont les options accessibles"""
 )
 
-plotting_demo()
+st.write(
+    """Nous allons voir ici comment le code fonctionne et quels sont les options accessibles"""
+)
 
-show_code(plotting_demo)
+# plotting_demo()
+
+# show_code(plotting_demo)

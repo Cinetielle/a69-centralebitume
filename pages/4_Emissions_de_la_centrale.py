bc336726102898725b6f2ee6bb21874a6c2f9d29
cinetielle <mathieubellanger@gmail.com>
2024-01-21 12:42:32 +0100

    modifié :         DATA/DATA_CAREPS.csv 	modifié :         pages/4_Emissions_de_la_centrale.py

--- pages/4_Emissions_de_la_centrale.py
+++ pages/4_Emissions_de_la_centrale.py
@@ -27,45 +27,92 @@ def CAREPS():
     st.markdown("""## Les données du CAREPS
                 
                 Les données de l'étude du CAREPS ont été numérisées. Nous présentons ici des histogrammes de distribution des principales qualités des émissions.""", unsafe_allow_html=True)
+    run = st.checkbox("Voir les données")
+    if run :
+        humidité = data_CAREPS['Humidité (%)'].to_numpy()
+        fig, ax =  plt.subplots(1, 2, figsize=(10, 4))
+        ax[0].hist(humidité, color="k", bins=30)
+        ax[0].set_title("Humidité (%)")
+        ax[1].boxplot(humidité[~np.isnan(humidité)])
+        st.pyplot(fig)
     
+        O2 = data_CAREPS['O2 (%)'].to_numpy()
+        fig, ax =  plt.subplots(1, 2, figsize=(10, 4))
+        ax[0].hist(O2, color="k", bins=30)
+        ax[0].set_title("O2 (%)")
+        ax[1].boxplot(O2[~np.isnan(O2)])
+        st.pyplot(fig)
+    
+        T = data_CAREPS['T° moyenne (°C)'].to_numpy()
+        fig, ax =  plt.subplots(1, 2, figsize=(10, 4))
+        ax[0].hist(T, color="k", bins=30)
+        ax[0].set_title("Température moyenne (°C)")
+        ax[1].boxplot(T[~np.isnan(T)])
+        st.pyplot(fig)
+    
+        v = data_CAREPS['v éjection gaz (m/s)'].to_numpy()
+        fig, ax =  plt.subplots(1, 2, figsize=(10, 4))
+        ax[0].hist(v, color="k", bins=30)
+        ax[0].set_title("Vitesse d'émission (m/s)")
+        ax[1].boxplot(v[~np.isnan(v)])
+        st.pyplot(fig)
+    
+        fig, ax =  plt.subplots(figsize=(10, 4))
+        f=ax.scatter(v, T, c=O2, s=humidité, cmap='jet')
+        plt.colorbar(f, ax=ax).set_label("O2 (%) \n (la taille reflète l'humidité)")
+        ax.set_xlabel("Vitesse d'émission (m/s)")
+        ax.set_ylabel('Température (°C)')
+        st.pyplot(fig)
+        
+        RegBru = data_CAREPS['régime tube et brûleur (%)'].to_numpy()
+        fig, ax =  plt.subplots(figsize=(10, 4))
+        f=ax.scatter(O2, RegBru, c=T, s=humidité, cmap='jet')
+        plt.colorbar(f, ax=ax).set_label("Température (°C)\n (la taille reflète l'humidité)")
+        ax.set_xlabel('O2 (%) ')
+        ax.set_ylabel('Régime des tubes et bruleurs (%)')
+        st.pyplot(fig)
+        
+        DebEnr = data_CAREPS['débit des enrobés (t/h)'].to_numpy()/data_CAREPS['débit nominal (t/h)'].to_numpy()
+        fig, ax =  plt.subplots(figsize=(10, 4))
+        f=ax.scatter(DebEnr, RegBru, c=O2, s=humidité, cmap='jet')
+        plt.colorbar(f, ax=ax).set_label("O2 (%) \n (la taille reflète l'humidité)")
+        ax.set_xlabel('débit des enrobés (t/h) /  débit nominal de la centrale (t/h)')
+        ax.set_ylabel('Régime des tubes et bruleurs (%)')
+        st.pyplot(fig)
+    
+        DebGazNorm = data_CAREPS['débit gaz (Nm3/h)'].to_numpy()
+        fig, ax =  plt.subplots(figsize=(10, 4))
+        f=ax.scatter(DebGazNorm, v, c=O2, s=humidité, cmap='jet')
+        plt.colorbar(f, ax=ax).set_label("O2 (%) \n (la taille reflète l'humidité)")
+        ax.set_xlabel('débit gaz normalisé (Nm3/h)')
+        ax.set_ylabel("Vitesse d'émission (m/s)")
+        st.pyplot(fig)
+        
+        DebGazNorm = data_CAREPS['débit gaz (Nm3/h)'].to_numpy()
+        fig, ax =  plt.subplots(figsize=(10, 4))
+        f=ax.scatter(DebGazNorm, v, c=T, s=humidité, cmap='jet')
+        plt.colorbar(f, ax=ax).set_label("Température (°C) \n (la taille reflète l'humidité)")
+        ax.set_xlabel('débit gaz normalisé (Nm3/h)')
+        ax.set_ylabel("Vitesse d'émission (m/s)")
+        st.pyplot(fig)
+        
+        fig, ax =  plt.subplots(figsize=(10, 4))
+        f=ax.scatter(DebGazNorm, T, c=O2, s=humidité, cmap='jet')
+        plt.colorbar(f, ax=ax).set_label("O2 (%) \n (la taille reflète l'humidité)")
+        ax.set_xlabel('débit gaz normalisé (Nm3/h)')
+        ax.set_ylabel("Température (°C) ")
+        st.pyplot(fig)
+    
+        st.write(data_CAREPS.to_html(escape=False, index=False), unsafe_allow_html=True)
 
-    humidité = data_CAREPS['Humidité (%)'].to_numpy()
-    fig, ax =  plt.subplots(1, 2, figsize=(5, 2))
-    ax[0].hist(humidité, color="k", bins=30)
-    ax[0].set_title("Humidité (%)")
-    ax[1].boxplot(humidité[~np.isnan(humidité)])
-    st.pyplot(fig)
-
-    O2 = data_CAREPS['O2 (%)'].to_numpy()
-    fig, ax =  plt.subplots(1, 2, figsize=(5, 2))
-    ax[0].hist(O2, color="k", bins=30)
-    ax[0].set_title("O2 (%)")
-    ax[1].boxplot(O2[~np.isnan(O2)])
-    st.pyplot(fig)
-
-    T = data_CAREPS['T° moyenne (°C)'].to_numpy()
-    fig, ax =  plt.subplots(1, 2, figsize=(5, 2))
-    ax[0].hist(T, color="k", bins=30)
-    ax[0].set_title("Température moyenne (°C)")
-    ax[1].boxplot(T[~np.isnan(T)])
-    st.pyplot(fig)
-
-    v = data_CAREPS['v éjection gaz (m/s)'].to_numpy()
-    fig, ax =  plt.subplots(1, 2, figsize=(5, 2))
-    ax[0].hist(v, color="k", bins=30)
-    ax[0].set_title("Vitesse d'émission (m/s)")
-    ax[1].boxplot(v[~np.isnan(v)])
-    st.pyplot(fig)
-
-    fig, ax =  plt.subplots(figsize=(5, 2))
-    f=ax.scatter(v, T, c=O2, s=humidité, cmap='jet')
-    plt.colorbar(f, ax=ax).set_label('O2 (%)')
-    ax.set_xlabel("Vitesse d'émission (m/s)")
-    ax.set_ylabel('Température (°C)')
-    st.pyplot(fig)
-
-    st.write(data_CAREPS.to_html(escape=False, index=False), unsafe_allow_html=True)
-
+def Normalisation():
+    st.markdown("""## La normalisation
+                
+                Les concentrations réglementaires sont données pour des valeurs de débits normalisés. Nous allons détailler ici le calcul de normalisation.""", unsafe_allow_html=True)
+    run = st.checkbox("Voir les calculs de normalisation")
+    if run :
+        st.markdown("""La première étape""", unsafe_allow_html=True)
+    
 def main():
 
 
@@ -101,3 +148,5 @@ st.markdown(
 )
 
 CAREPS()
+
+Normalisation()
